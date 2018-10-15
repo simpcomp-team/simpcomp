14b8236a5f4904c22fcdad194f5e963e4f7b8c23
Jonathan <jonathan.spreer@fu-berlin.de>
2018-10-15 15:38:36 +0200

    added autogen.sh

---
+++ autogen.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -ex
+#
+# simpcomp package for GAP
+#
+# This file is part of the build system of a GAP kernel extension.
+# Requires GNU autoconf, GNU automake and GNU libtool.
+#
+autoreconf -vif `dirname "$0"`
+
